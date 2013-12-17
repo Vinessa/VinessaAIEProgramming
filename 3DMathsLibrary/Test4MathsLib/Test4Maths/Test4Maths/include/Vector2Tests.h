5a2e39e7e9bf455a1c13f5c11f68b5c6d7053bd3
Vinessa <Vinessa.Mayer@Students.aie.edu.au>
2013-12-17 15:25:56 -0800

    filling out test program for v2
    filling out test program for v2

--- 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/include/Vector2Tests.h
+++ 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/include/Vector2Tests.h
@@ -3,27 +3,84 @@
 #include <iostream>
 using namespace std;
 
+enum ALG
+{
+	ADDS,
+	ADDV,
+	SUBS,
+	SUBV,
+	MULTS,
+	DIVS,
+	GETMAG,
+	NORM,
+	GETNORM,
+	EULER,
+	GETDOTPROD,
+	XPROD,
+	LININT
+};
+
+
 class Vector2Tests
 {
 public:
 	Vector2Tests(void);
 	~Vector2Tests(void);
 
+	float dt;
 
 	Vector2 T1;
 	Vector2 T2;
 	Vector2 T3;
 	Vector2 ExpResult;
+
+	Vector2 XResAddS;
+	Vector2 XResAddV;
+	Vector2 XResSubS;
+	Vector2 XResSubV;
+	Vector2 XResMultS;
+	Vector2 XResDivS;
+	float XResGetMAG;
+	Vector2 XResNorm;
+	Vector2 XResGetNorm;
+	float XResEuler;
+	float XResGetDotProd;
+	
+	Vector2 XResLinInt;
+
 	Vector2 Result;
-	bool PassFail;
+	float Resultf;
+
+	bool PassFailAddS;
+	bool PassFailAddV;
+	bool PassFailSubS;
+	bool PassFailSubV;
+	bool PassFailMultS;
+	bool PassFailDivS;
+	bool PassFailGetMAG;
+	bool PassFailNorm;
+	bool PassFailGetNorm;
+	bool PassFailEuler;
+	bool PassFailGetDotProd;
+	bool PassFailXProd;
+	bool PassFailLinInt;
 
 	float TScaler;
 
 
 
 	void SetData (float FirstVecX, float FirstVecY, float SecondVecX, float SecondVecY, float ExpResultVecX, float ExpResultVecY);
-	virtual bool DoTest();
-	void Vector2Tests::TestResult();
+	bool DoTestAddVector();
+	bool DoTestAddScaler();
+	bool DoTestMultScaler();
+	bool DoTestDivScaler();
+
+	bool DoTestGetMag();
+
+	bool Vector2Tests::DoTestSubVector();
+	bool DoTestSubScaler();
+	void ReportTestResult(char a_WhichTest);
+	
 };
 
 
--- 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/source/Vector2Tests.cpp
+++ 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/source/Vector2Tests.cpp
@@ -15,35 +15,181 @@ Vector2Tests::~Vector2Tests(void)
 void Vector2Tests::SetData (float FirstVecX, float FirstVecY, float SecondVecX, float SecondVecY, float ExpResultVecX, float ExpResultVecY)
 
 {
+	T1.SetX(1);
+	T1.SetY(2);
 
-	T1.SetX(FirstVecX);
+	T2.SetX(3);
+	T2.SetY(4);
+	
+	T3.SetX(5);
+	T3.SetY(6);
+
+	TScaler = 9;
+	dt = .5;
+
+	XResAddS.SetX(10);
+	XResAddS.SetY(11);
+
+	XResAddV.SetX(4);
+	XResAddV.SetY(6);
+
+	XResSubS.SetX(-8);
+	XResSubS.SetY(-7);
+
+	XResSubV.SetX(-2);
+	XResSubV.SetY(-2);
+
+	XResMultS.SetX(9);
+	XResMultS.SetY(18);
+	
+	XResDivS.SetX(0.11);
+	XResDivS.SetY(0.22);
+
+	XResGetMAG = 2.24;
+
+
+	XResGetNorm.SetX(0.45);
+	XResGetNorm.SetY(0.89);
+	
+	XResEuler = 0.18;
+
+	XResGetDotProd = 11;
+
+	XResLinInt.SetX(2);
+	XResLinInt.SetX(3.5);
+
+
+
+
+	/*T1.SetX(FirstVecX);
 	T1.SetY(FirstVecY);
 
 	T2.SetX(SecondVecX);
 	T2.SetY(SecondVecY);
 
 	ExpResult.SetX(ExpResultVecX);
-	ExpResult.SetY(ExpResultVecY);
+	ExpResult.SetY(ExpResultVecY);*/
 
 
 
 }
 
-bool Vector2Tests::DoTest()
+bool Vector2Tests::DoTestAddScaler()
+{
+	cout << "Testing VECTOR + SCALER ... \n";
+	Result = (T1 + TScaler);
+	return (XResAddS == Result);
+}
+
+bool Vector2Tests::DoTestAddVector()
 {
 	cout << "Testing VECTOR + VECTOR ... \n";
 	Result = (T1 + T2);
-	return (ExpResult == Result);
+	return (XResAddV == Result);
 	
 }
 
-void Vector2Tests::TestResult()
+bool Vector2Tests::DoTestSubScaler()
 {
-	PassFail = (DoTest());
-	if (PassFail != true)
+	cout << "Testing VECTOR - Scaler ... \n";
+	Result = (T1 - TScaler);
+	return (XResSubS == Result);
+	
+}
+
+bool Vector2Tests::DoTestSubVector()
+{
+	cout << "Testing VECTOR - VECTOR ... \n";
+	Result = (T1 - T2);
+	return (XResSubV == Result);
+	
+}
+
+bool Vector2Tests::DoTestMultScaler()
+{
+	cout << "Testing VECTOR * SCALER ... \n";
+	Result = (T1 * TScaler);
+	return (XResMultS == Result);
+	
+}
+
+bool Vector2Tests::DoTestDivScaler()
+{
+cout << "Testing VECTOR / SCALER ... \n";
+	Result = (T1 / TScaler);
+	return (XResDivS == Result);
+	
+}
+
+bool Vector2Tests::DoTestGetMag()
+{
+cout << "Testing GetMagnitude ... \n";
+	Resultf = (T1.GetMagnitude2D());
+	return (XResGetMAG == Resultf);
+	
+}
+
+
+
+
+
+void Vector2Tests::ReportTestResult(char a_WhichTest)
+{
+	
+	if (a_WhichTest != true)
 		cout << "----------FAILED------: \n Expected: " << ExpResult.GetX() << ","<< ExpResult.GetY() << "\n Got: " << Result.GetX() << "," <<Result.GetY() << "\n";
 	else
-		if (PassFail == true)
+		if (a_WhichTest == true)
 			cout << "Passed: \n Expected: " << ExpResult.GetX() << ","<< ExpResult.GetY() << "\n Got: " << Result.GetX() << "," <<Result.GetY() << "\n \n";
 
 }
+
+
+
+void Vector2Tests::TestAll(ALG)
+{
+	switch(ALG)
+	{
+	case ADDS:
+		DoTestAddVector();
+		TestResult(DoTestAddVector);
+		break;
+
+	case ADDV:
+
+		break;
+	case SUBS:
+
+		break;
+	case SUBV:
+
+		break;
+	case MULTS:
+
+		break;
+	case DIVS:
+
+		break;
+	case GETMAG:
+
+		break;
+	case NORM:
+
+		break;
+	case GETNORM:
+
+		break;
+	case EULER:
+
+		break;
+	case GETDOTPROD:
+
+		break;
+	case XPROD:
+
+		break;
+	case LININT:
+
+		break;
+
+	}
