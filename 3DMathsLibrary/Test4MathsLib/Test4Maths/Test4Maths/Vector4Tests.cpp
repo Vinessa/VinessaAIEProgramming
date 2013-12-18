90df36e5a12ab832c440799233e534dccf3def9b
Vinessa <Vinessa.Mayer@Students.aie.edu.au>
2013-12-18 12:42:35 -0800

    Begining vector4 tests (DNC)

---
+++ 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/Vector4Tests.cpp
@@ -0,0 +1,80 @@
+#include "Vector4Tests.h"
+
+
+Vector4Tests::Vector4Tests(void)
+{
+}
+
+
+Vector4Tests::~Vector4Tests(void)
+{
+}
+
+void Vector4Tests::SetData ()
+
+{
+	T1.m_X = 1;
+	T1.m_Y = 2;
+	T1.m_Z = 3;
+	T1.m_A = 1;
+
+	T2.m_X = 4;
+	T2.m_Y = 5;
+	T2.m_Z = 6;
+	T2.m_A = 1;
+
+	T3.m_X = 7;
+	T3.m_Y = 8;
+	T3.m_Z = 9;
+	T3.m_A = 1;
+
+	TA.m_Red = 1;
+	TA.m_Green = 2;
+	TA.m_Blue = 2;
+	TA.m_Alpha = 1;
+
+	TScaler = 9;
+	dt = .5;
+
+	/*XResAddS.SetX(10);
+	XResAddS.SetY(11);
+
+	XResAddV.SetX(4);
+	XResAddV.SetY(6);
+
+	XResSubS.SetX(-8);
+	XResSubS.SetY(-7);
+
+	XResSubV.SetX(-2);
+	XResSubV.SetY(-2);
+
+	XResMultS.SetX(9);
+	XResMultS.SetY(18);*/
+	
+	/*XResDivS.SetX(0.11);
+	XResDivS.SetY(0.22);*/
+
+	XResGetMAG = 2.236;
+
+
+	//XResGetNorm.SetX(0.45);
+	//XResGetNorm.SetY(0.89);
+	
+	XResEuler = 0.18;
+
+	XResGetDotProd = 11;
+
+	/*XResLinInt.SetX(2);
+	XResLinInt.SetX(3.5);*/
+
+	AddVec = "AddVector";
+	AddScaler = "AddbyScaler";
+	SubtractVec = "SubtractVector";
+	SubtractScaler = "SubtractbyScaler";
+	MultScaler = "MultiplybyScaler";
+	DivScaler = "DividebyScaler";
+	GetMAG = "GetMagnitude";
+	Normalize = "Normalize";
+	EulerAngle = "EulerAngle";
+	GetDotProduct = "GetDotProduct";
+	LinearInterpolation = "LinearInterpolation";
---
+++ 3DMathsLibrary/Test4MathsLib/Test4Maths/Test4Maths/Vector4Tests.h
@@ -0,0 +1,137 @@
+#pragma once
+class Vector4Tests
+{
+public:
+	Vector4Tests(void);
+	~Vector4Tests(void);
+};
+
+#pragma once
+#include "Vector4.h"
+#include <iostream>
+using namespace std;
+
+
+
+class Vector4Tests
+{
+public:
+	Vector4Tests(void);
+	~Vector4Tests(void);
+
+//CONSTANT VECTORS AND VARIABLES//
+	//(INGREDIENTS)
+
+	float dt;
+
+	Vector4 T1;
+	Vector4 T2;
+	Vector4 T3;
+	Vector4 TA;
+
+	float TScaler;
+
+	//TEMPS////////
+	Vector4 Result;
+	float Resultf;
+	Vector4 ExpResult;
+
+//EXPECTED RESULTS///////////////
+
+	Vector4 XResAddS;
+	Vector4 XResAddV;
+	Vector4 XResSubS;
+	Vector4 XResSubV;
+	Vector4 XResMultS;
+	Vector4 XResDivS;
+	float XResGetMAG;
+	Vector4 XResNorm;
+	Vector4 XResGetNorm;
+	float XResEuler;
+	Vector4 XResCrossProduct;
+	float XResGetDotProd;
+	Vector4 XResLinInt;
+	Vector4 XResLinIntVec;
+
+
+//ACTUAL RESULTS/////////////////
+	Vector4 ResAddS;
+	Vector4 ResAddV;
+	Vector4 ResSubS;
+	Vector4 ResSubV;
+	Vector4 ResMultS;
+	Vector4 ResDivS;
+	float ResGetMAG;
+	Vector4 ResNorm;
+	Vector4 ResGetNorm;
+	float ResEuler;
+	Vector4 ResCrossProduct;
+	float ResGetDotProd;
+	Vector4 ResLinInt;
+
+
+//PASS OR FAILS////////////////////
+	bool PassFailAddS;
+	bool PassFailAddV;
+
+	bool PassFailSubS;
+	bool PassFailSubV;
+
+	bool PassFailMultS;
+	bool PassFailDivS;
+
+	bool PassFailGetMAG;
+	bool PassFailNorm;
+	bool PassFailGetNorm; //only tested this instead of normalize because get normal has normalize in it and will test both
+	bool PassFailEuler;
+	bool PassFailXProd;
+	bool PassFailGetDotProd;
+	bool PassFailLinInt;
+
+
+//FUNCTIONS////////////////////////
+	void SetData ();
+
+	bool DoTestAddVector();
+	bool DoTestAddScaler();
+
+	bool DoTestSubVector();
+	bool DoTestSubScaler();
+
+	bool DoTestMultScaler();
+	bool DoTestDivScaler();
+
+	bool DoTestGetMag();
+	bool DoTestNormalize();
+	bool DoTestEuler();
+	bool DoTestXProd();
+	bool DoTestGetDotProd();
+	bool DoTestLinInt();
+
+	void ReportTestResultFloat(bool a_WhichBool, float a_WhichResultf, float a_WhichExpectedf, char* a_Test);
+	void ReportTestResultVec(bool a_WhichBool, Vector4 a_WhichResult, Vector4 a_WhichExpected, char* a_Test);
+	void AssignTestResults();
+	void RunTestAll();
+
+	//LABELS///
+
+	char* AddVec;
+	char* AddScaler;
+
+	char* SubtractVec;
+	char* SubtractScaler;
+
+	char* MultScaler;
+	char* DivScaler;
+
+	char* GetMAG;
+	char* Normalize;
+	char* EulerAngle;
+	char* CrossProd;
+	char* GetDotProduct;
+	char* LinearInterpolation;
+	
+};
+
+
+
