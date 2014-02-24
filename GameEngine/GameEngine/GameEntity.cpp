d8a1f6a93a6446b59939b0be008c647b78b83fe6
Vinessa <Vinessa.Mayer@Students.aie.edu.au>
2014-02-24 13:32:40 -0800

    Revert "gdbc"
    This reverts commit e19d5b59eaeff39ce0d75a67d92ad3320d30185b.

--- GameEngine/GameEngine/GameEntity.cpp
+++ GameEngine/GameEngine/GameEntity.cpp
@@ -5,8 +5,8 @@ GameEntity::GameEntity(void)
 {
 }
 
-GameEntity::GameEntity(const char* a_pTexture, int a_iWidth, int a_iHeight, tbyte::Vector4 a_v4Color,GLFWwindow* Window)
-	:Sprite(a_pTexture, a_iWidth, a_iHeight, a_v4Color, Window)
+GameEntity::GameEntity(const char* a_pTexture, int a_iWidth, int a_iHeight, tbyte::Vector4 a_v4Color,GLFWwindow* Window,float a_AniOffsetU,float a_AniOffsetV,float Z_Position, tbyte::Vector3 UVCoord1,tbyte::Vector3 UVCoord2,tbyte::Vector3 UVCoord3,tbyte::Vector3 UVCoord4)
+	:Sprite(a_pTexture, a_iWidth, a_iHeight, a_v4Color, Window, float a_AniOffsetU,float a_AniOffsetV,float Z_Position, tbyte::Vector3 UVCoord1,tbyte::Vector3 UVCoord2,tbyte::Vector3 UVCoord3,tbyte::Vector3 UVCoord4)
 {
 	
 }
--- GameEngine/GameEngine/Include/GameEntity.h
+++ GameEngine/GameEngine/Include/GameEntity.h
@@ -7,7 +7,7 @@ public:
 
 
 	GameEntity(void);
-	GameEntity(const char* a_TextureName, int a_iEntityWidth, int a_iEntityHeight, tbyte::Vector4 a_v4MaterialColor, GLFWwindow* Window);
+	GameEntity(const char* a_TextureName, int a_iEntityWidth, int a_iEntityHeight, tbyte::Vector4 a_v4MaterialColor, GLFWwindow* Window, float a_AniOffsetU,float a_AniOffsetV,float Z_Position, tbyte::Vector3 UVCoord1,tbyte::Vector3 UVCoord2,tbyte::Vector3 UVCoord3,tbyte::Vector3 UVCoord4);
 	~GameEntity(void);
 
 	void Move();
